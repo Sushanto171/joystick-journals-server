6b2d67361be24dca644d12cc42ec2cc8f99f3ca4
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2024-12-04 19:20:31 +0600

    put operation,log in by third party user data

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
-// JoySitck-journals
-// LgAsJ5AMWoX90w6O
-const key = process.env.key;
-const pass = process.env.pass;
-console.log({ key, pass });
-const uri =
-  "mongodb+srv://<db_username>:<db_password>@cluster0.0zizn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // middleware
 app.use(express.json());
 app.use(cors());
+
+// const uri = `mongodb+srv://${process.env.key}:${process.env.pass}@cluster0.0zizn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb://localhost:27017/";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  const usersCollection = client.db("joyStick-journalsDB").collection("users");
+
+  try {
+    // users api
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const data = req.body;
+      // console.log(data, email);
+      const filter = { email: email };
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: data,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        option
+      );
+      res.send(result);
+    });
+
+    // comment out must
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("JoyStick Journals Running On....");
